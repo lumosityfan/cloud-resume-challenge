88594e20f6a0505fa96ed94d3369434cacdc915d
Jeff Xie <jeffxie4@gmail.com>
2026-03-19 14:56:26 -0400

    adding github configurations for GitHub Actions and adding JavaScript for future API Gateway/Lambda invocation

---
+++ script.js
@@ -0,0 +1,18 @@
+// Example using fetch in your static site's JavaScript
+const apiEndpoint = 'https://p05vz9vxlc.execute-api.us-east-2.amazonaws.com/';
+const data = { 
+    counter: 1,
+    id: 'unique-id-12345',
+    name: 'John Doe',
+ };
+
+fetch(apiEndpoint, {
+    method: 'GET', // or 'POST' if you want to send data
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => console.log('Success:', data))
+.catch((error) => console.error('Error:', error));
