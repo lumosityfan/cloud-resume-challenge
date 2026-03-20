afeddb3143a31a26478f50e28b9f513eeb5d3cfd
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2026-03-20 14:53:40 -0400

    testing accessing apiEndpoint GET visitor-counter for initial data access

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // Example using fetch in your static site's JavaScript
-const apiEndpoint = 'https://p05vz9vxlc.execute-api.us-east-2.amazonaws.com/';
-const data = { 
-    counter: 1,
-    id: 'visitor-counter',
-    name: 'Website Visitor Counter',
- };
+const apiEndpoint = 'https://p05vz9vxlc.execute-api.us-east-2.amazonaws.com/visitor-counter';
+// const data = { 
+//     counter: 1,
+//     id: 'visitor-counter',
+//     name: 'Website Visitor Counter',
+//  };
 
 fetch(apiEndpoint, {
-    method: 'POST', // or 'POST' if you want to send data
+    method: 'GET', // or 'POST' if you want to send data
     headers: {
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
 })
-.then(response => response.json())
-.then(data => console.log('Success:', data))
+.then(response => console.log('Response:', response))
 .catch((error) => console.error('Error:', error));
