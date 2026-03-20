4915abb90e33d2fb27cbbae7896b3627b29290e3
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2026-03-20 16:23:42 -0400

    remove unnecessary console.log invocations

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Example using fetch in your static site's JavaScript
 const apiEndpointGet = 'https://p05vz9vxlc.execute-api.us-east-2.amazonaws.com/visitor-counter';
 const apiEndpointPost = 'https://p05vz9vxlc.execute-api.us-east-2.amazonaws.com/';
-// let visitorCountObject;
 
 async function fetchAndPost() {
     try {
@@ -9,8 +8,6 @@ async function fetchAndPost() {
         const getResponse = await fetch(apiEndpointGet);
         if (!getResponse.ok) throw new Error(`GET request failed: ${getResponse.statusText}`);
         const data = await getResponse.json();
-
-        console.log('GET Success:', data);
         
         // 2. POST request
         const newVisitorCount = data[0].counter + 1;
@@ -26,14 +23,13 @@ async function fetchAndPost() {
         if (!postResponse.ok) throw new Error(`POST request failed: ${postResponse.statusText}`);
         const postData = await postResponse.json();
 
-        console.log('POST Success:', postData);
         document.getElementById('visitor-count').textContent = newVisitorCount;
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-fetchAndPost().then(() => console.log('Visitor count updated successfully.'))
+fetchAndPost();
 
 // // Get initial visitor count
 // fetch(apiEndpointGet, {
