7b9d9cb763f2a0df502c069353d09ba4d9cc0682
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2026-04-03 13:38:29 -0400

    Removed need to do separate post response because of changes in Lambda file

--- visitor_counter.js
+++ visitor_counter.js
@@ -3,27 +3,10 @@ const apiEndpoint = 'https://brdnoy7rm9.execute-api.us-east-2.amazonaws.com/visi
 
 async function fetchAndPost() {
     try {
-        // 1. GET requst
-        const getResponse = await fetch(apiEndpoint);
-        if (!getResponse.ok) throw new Error(`GET request failed: ${getResponse.statusText}`);
-        const data = await getResponse.json();
-        print(data)
-        
-        // 2. POST request
-        const newVisitorCount = data[0].counter + 1;
-        const postResponse = await fetch(apiEndpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                counter: newVisitorCount,
-                id: data[0].id,
-                name: data[0].name 
-            }),
-        });
-        if (!postResponse.ok) throw new Error(`POST request failed: ${postResponse.statusText}`);
-        const postData = await postResponse.json();
-
-        document.getElementById('visitor-count').textContent = newVisitorCount;
+        const response = await fetch(apiEndpoint);
+        if (!response.ok) throw new Error(`Request failed: ${response.statusText}`);
+        const data = await response.json();
+        document.getElementById('visitor-count').textContent = data[0].counter;
     } catch (error) {
         console.error('Error:', error);
     }
