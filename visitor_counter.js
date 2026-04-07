5ad5f3a1bc125f1415de9ceb2772e03399bfd185
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2026-04-07 14:14:23 -0400

    Adjusted apiEndpoints and added section for if no entry from fetch initially

--- visitor_counter.js
+++ visitor_counter.js
@@ -1,12 +1,19 @@
 // Example using fetch in your static site's JavaScript
-const apiEndpointGet = 'https://w0krslxy78.execute-api.us-east-2.amazonaws.com/visitorCount';
-const apiEndpointPost = 'https://w0krslxy78.execute-api.us-east-2.amazonaws.com/visitorCount/increment';
+const apiEndpointGet = 'https://xt9kqsik74.execute-api.us-east-2.amazonaws.com/visitorCount';
+const apiEndpointPost = 'https://xt9kqsik74.execute-api.us-east-2.amazonaws.com/visitorCount/increment';
 
 async function fetchAndPost() {
     try {
         const response = await fetch(apiEndpointGet);
         if (!response.ok) throw new Error(`Request failed: ${response.statusText}`);
         const data = await response.json();
+        if (data.length === 0) {
+            data[0] = {
+                counter: 0,
+                id: "visitor-counter",
+                name: "Visitor Counter"
+            }
+        }
         const postResponse = await fetch(apiEndpointPost, { method: 'POST', 
                                                             headers: { 'Content-Type': 'application/json' }, 
                                                             body: JSON.stringify(data[0]) });
